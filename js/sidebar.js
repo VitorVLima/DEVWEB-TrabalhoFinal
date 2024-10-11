1e6fdee631c0275ee0a7dda6aef688b97946553e
VITOR VINICIUS LIMA SILVA <vitor14_df@hotmail.com>
2024-10-11 15:28:49 -0300

    vitor

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,6 +1,8 @@
 const menuButton = document.getElementById('menuButton');
 const closeMenu = document.getElementById('closeMenu');
-const sidebar = document.getElementById('sidebar')
+const sidebar = document.getElementById('sidebar');
+const destinosButton = document.getElementById('destinos');
+const sidebarSubItems = document.getElementById('sidebarSubItems');
 
 menuButton.addEventListener('click', function(){
     sidebar.style.display = 'block';
@@ -9,11 +11,24 @@ menuButton.addEventListener('click', function(){
 
 closeMenu.addEventListener('click', function(){
     sidebar.style.display = 'none';
+    sidebarSubItems.style.display = 'none';
     //esconde a sidebar ao clicar em fechar
 });
+
+destinosButton.addEventListener('click', function(){
+    if(sidebarSubItems.style.display == 'none'){
+        sidebarSubItems.style.display = 'flex';
+    }else{
+        sidebarSubItems.style.display = 'none';
+    }
+});
+
+
 
 window.addEventListener('resize', function() {
     if (window.innerWidth > 769) {
         sidebar.style.display = 'none'; // Esconde a sidebar se a largura for maior que 769px
+        sidebarSubItems.style.display = 'none'; // subitens da sidebar são escondidos 
     }
 });
+

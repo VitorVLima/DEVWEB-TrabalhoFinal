6910f7d375d6bb21d1aad102c618776983baa25e
VITOR VINICIUS LIMA SILVA <vitor14_df@hotmail.com>
2024-10-12 12:30:21 -0300

    VITOR

---
+++ js/script.js
@@ -0,0 +1,44 @@
+async function loadHTML(elementId, file) {
+    const response = await fetch(file);
+    const text = await response.text();
+    document.getElementById(elementId).innerHTML = text;
+    sidebar();
+}
+
+function sidebar(){
+    document.addEventListener("DOMContentLoaded", function() {
+        const menuButton = document.getElementById('menuButton');
+        const closeMenu = document.getElementById('closeMenu');
+        const sidebar = document.getElementById('sidebar');
+        const destinosButton = document.getElementById('destinos');
+        const sidebarSubItems = document.getElementById('sidebarSubItems');
+    
+        // Inicializa o estilo de exibição da sidebarSubItems
+        sidebarSubItems.style.display = 'none';
+    
+        menuButton.addEventListener('click', function() {
+            sidebar.style.display = 'block'; // Abre a sidebar
+        });
+    
+        closeMenu.addEventListener('click', function() {
+            sidebar.style.display = 'none'; // Esconde a sidebar
+            sidebarSubItems.style.display = 'none'; // Esconde subitens
+        });
+    
+        destinosButton.addEventListener('click', function() {
+            // Alterna a exibição dos subitens
+            sidebarSubItems.style.display = (sidebarSubItems.style.display === 'none' || sidebarSubItems.style.display === '') ? 'flex' : 'none';
+        });
+    
+        window.addEventListener('resize', function() {
+            if (window.innerWidth > 769) {
+                sidebar.style.display = 'none'; // Esconde a sidebar em telas grandes
+                sidebarSubItems.style.display = 'none'; // Esconde subitens da sidebar
+            }
+        });
+    });
+}
+
+loadHTML('iheader', 'estrutura/header.html');
+loadHTML('isidebar', 'estrutura/sidebar.html');
+loadHTML('ifooter', 'estrutura/footer.html');

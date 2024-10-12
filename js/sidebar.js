79c7b40ead9f0801b72e91c55fae9a09986ed225
VITOR VINICIUS LIMA SILVA <vitor14_df@hotmail.com>
2024-10-11 23:48:33 -0300

    Update sidebar.js

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,38 +1,31 @@
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener("DOMContentLoaded", function() {
+    const menuButton = document.getElementById('menuButton');
+    const closeMenu = document.getElementById('closeMenu');
+    const sidebar = document.getElementById('sidebar');
+    const destinosButton = document.getElementById('destinos');
+    const sidebarSubItems = document.getElementById('sidebarSubItems');
 
-const menuButton = document.getElementById('menuButton');
-const closeMenu = document.getElementById('closeMenu');
-const sidebar = document.getElementById('sidebar');
-const destinosButton = document.getElementById('destinos');
-const sidebarSubItems = document.getElementById('sidebarSubItems');
-
-menuButton.addEventListener('click', function(){
-    sidebar.style.display = 'block';
-    //abre a sidebar ao clicar no icone de menu
-});
-
-closeMenu.addEventListener('click', function(){
-    sidebar.style.display = 'none';
+    // Inicializa o estilo de exibição da sidebarSubItems
     sidebarSubItems.style.display = 'none';
-    //esconde a sidebar ao clicar em fechar
-});
 
-destinosButton.addEventListener('click', function(){
-    if(sidebarSubItems.style.display == 'none'){
-        sidebarSubItems.style.display = 'flex';
-    }else{
-        sidebarSubItems.style.display = 'none';
-    }
-});
+    menuButton.addEventListener('click', function() {
+        sidebar.style.display = 'block'; // Abre a sidebar
+    });
 
+    closeMenu.addEventListener('click', function() {
+        sidebar.style.display = 'none'; // Esconde a sidebar
+        sidebarSubItems.style.display = 'none'; // Esconde subitens
+    });
 
+    destinosButton.addEventListener('click', function() {
+        // Alterna a exibição dos subitens
+        sidebarSubItems.style.display = (sidebarSubItems.style.display === 'none' || sidebarSubItems.style.display === '') ? 'flex' : 'none';
+    });
 
-window.addEventListener('resize', function() {
-    if (window.innerWidth > 769) {
-        sidebar.style.display = 'none'; // Esconde a sidebar se a largura for maior que 769px
-        sidebarSubItems.style.display = 'none'; // subitens da sidebar são escondidos 
-    }
-});
-
-
+    window.addEventListener('resize', function() {
+        if (window.innerWidth > 769) {
+            sidebar.style.display = 'none'; // Esconde a sidebar em telas grandes
+            sidebarSubItems.style.display = 'none'; // Esconde subitens da sidebar
+        }
+    });
 });

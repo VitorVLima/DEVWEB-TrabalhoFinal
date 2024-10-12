07dca8a09da213617c87a7c581537bb1cd76b581
VITOR VINICIUS LIMA SILVA <vitor14_df@hotmail.com>
2024-10-12 16:55:44 -0300

    Create scripttedyr.js

---
+++ js/scripttedyr.js
@@ -0,0 +1,50 @@
+function loadHTML(elementId, filePath, callback) {
+    fetch(filePath)
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById(elementId).innerHTML = data;
+            if (callback) callback(); // Chama o callback, se existir
+        })
+        .catch(error => console.error('Erro ao carregar:', error));
+}
+
+window.onload = function() {
+    loadHTML('iheader', '../../estrutura/header.html');
+    loadHTML('isidebar', '../../estrutura/sidebar.html', initializeSidebar); // Passa a função de inicialização
+    loadHTML('ifooter', '../../estrutura/footer.html');
+};
+
+function initializeSidebar() {
+    const menuButton = document.getElementById('menuButton');
+    const closeMenu = document.getElementById('closeMenu');
+    const sidebar = document.getElementById('sidebar');
+    const destinosButton = document.getElementById('destinos');
+    const sidebarSubItems = document.getElementById('sidebarSubItems');
+
+    if (!menuButton || !closeMenu || !sidebar || !destinosButton || !sidebarSubItems) {
+        console.error('Um ou mais elementos não foram encontrados.');
+        return;
+    }
+
+    sidebarSubItems.style.display = 'none';
+
+    menuButton.addEventListener('click', function() {
+        sidebar.style.display = 'block';
+    });
+
+    closeMenu.addEventListener('click', function() {
+        sidebar.style.display = 'none';
+        sidebarSubItems.style.display = 'none';
+    });
+
+    destinosButton.addEventListener('click', function() {
+        sidebarSubItems.style.display = (sidebarSubItems.style.display === 'none' || sidebarSubItems.style.display === '') ? 'flex' : 'none';
+    });
+
+    window.addEventListener('resize', function() {
+        if (window.innerWidth > 769) {
+            sidebar.style.display = 'none';
+            sidebarSubItems.style.display = 'none';
+        }
+    });
+}

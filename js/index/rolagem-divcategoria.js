3f2ffbef24e2f34adce9b6cd16676987cd2053a2
VITOR VINICIUS LIMA SILVA <vitor14_df@hotmail.com>
2024-10-22 00:53:26 -0300

    vitor

--- js/index/rolagem-divcategoria.js
+++ js/index/rolagem-divcategoria.js
@@ -1,37 +1,30 @@
 const categoryContainer = document.getElementsByClassName('category');
 
-        for (let i = 0; i < categoryContainer.length; i++) {
-            // Rolagem com o Mouse
-            categoryContainer[i].addEventListener('wheel', function(event) {
-                event.preventDefault(); // Previne o comportamento padrão da rolagem
-                this.scrollLeft += event.deltaY; // Rola horizontalmente
-            });
+for (let i = 0; i < categoryContainer.length; i++) {
+    // Rolagem com o Mouse
+    categoryContainer[i].addEventListener('wheel', function(event) {
+        event.preventDefault(); // Previne o comportamento padrão da rolagem
+        this.scrollLeft += event.deltaY * 1.5; // Aumentar sensibilidade da rolagem
+    });
 
-            // Rolagem com o Toque
-            let startX;
-            let scrollLeft;
-            let isScrolling = false;
+    // Rolagem com o Toque
+    let startX;
+    let scrollLeft;
 
-            categoryContainer[i].addEventListener('touchstart', function(e) {
-                startX = e.touches[0].pageX - this.offsetLeft;
-                scrollLeft = this.scrollLeft;
-            });
+    categoryContainer[i].addEventListener('touchstart', function(e) {
+        startX = e.touches[0].pageX - this.offsetLeft;
+        scrollLeft = this.scrollLeft;
+    });
 
-            categoryContainer[i].addEventListener('touchmove', function(e) {
-                e.preventDefault(); // Previne o comportamento padrão do toque
-                const x = e.touches[0].pageX - this.offsetLeft;
-                const walk = (x - startX) * 2; // Ajuste a sensibilidade
-                const newScrollLeft = scrollLeft - walk;
+    categoryContainer[i].addEventListener('touchmove', function(e) {
+        e.preventDefault(); // Previne o comportamento padrão do toque
+        const x = e.touches[0].pageX - this.offsetLeft;
+        const walk = (x - startX) * 3; // Aumentar a sensibilidade da rolagem
+        const newScrollLeft = scrollLeft - walk;
 
-                // Tratamento de bordas
-                if (newScrollLeft >= 0 && newScrollLeft <= this.scrollWidth - this.clientWidth) {
-                    if (!isScrolling) {
-                        isScrolling = true;
-                        requestAnimationFrame(() => {
-                            this.scrollLeft = newScrollLeft;
-                            isScrolling = false;
-                        });
-                    }
-                }
-            });
+        // Tratamento de bordas
+        if (newScrollLeft >= 0 && newScrollLeft <= this.scrollWidth - this.clientWidth) {
+            this.scrollLeft = newScrollLeft; // Atualiza imediatamente
         }
+    });
+}

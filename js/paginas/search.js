3015502b1b00887be136b229bb14bc630c959664
VITOR VINICIUS LIMA SILVA <vitor14_df@hotmail.com>
2024-10-23 03:08:48 -0300

    vitor

--- js/paginas/search.js
+++ js/paginas/search.js
@@ -1,4 +1,5 @@
 const items = document.querySelectorAll('.items .item');
+const titles = document.querySelectorAll("h3 a");
 const noResults = document.getElementById('noResults');
 const resultMessage = document.getElementById('resultMessage');
 
@@ -16,9 +17,10 @@ function searchItems() {
     resultMessage.textContent = `Resultados encontrados para: "${result}"`;
 
     // Mostrar ou esconder itens
-    items.forEach(item => {
-        if (item.textContent.toLowerCase().trim().indexOf(result) !== -1) {
-            item.style.display = 'block';
+    titles.forEach((title, index) => {
+        const item = items[index]; // Relacionar titles com items
+        if (title.textContent.toLowerCase().trim().indexOf(result) !== -1) {
+            item.style.display = 'flex';
             hasResults = true;
         } else {
             item.style.display = 'none';
@@ -37,4 +39,5 @@ if (query) {
     resultMessage.textContent = 'Por favor, insira um destino para pesquisar.';
     noResults.style.display = 'block';
 }
+
 

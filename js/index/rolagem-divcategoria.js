9514c2f68fc0227a63d6898517355d2e2ff51264
VITOR VINICIUS LIMA SILVA <vitor14_df@hotmail.com>
2024-10-31 16:11:10 -0300

    vitor

--- js/index/rolagem-divcategoria.js
+++ js/index/rolagem-divcategoria.js
@@ -1,14 +1,25 @@
-const scrollButtons = document.querySelectorAll('.scroll-button');
+const categories = document.querySelectorAll('.category');
 
-    scrollButtons.forEach(button => {
-        button.addEventListener('click', function() {
-            const category = button.parentElement.querySelector('.category');
-            const scrollAmount = 190; // Quantidade de pixels para rolar
-
-            if (button.classList.contains('left')) {
-                category.scrollLeft -= scrollAmount; // Rolar para a esquerda
-            } else {
-                category.scrollLeft += scrollAmount; // Rolar para a direita
-            }
-        });
+categories.forEach(category => {
+    // Rolagem com o Mouse
+    category.addEventListener('wheel', function(event) {
+        event.preventDefault(); // Previne o comportamento padrão da rolagem
+        this.scrollLeft += event.deltaY * 1.5; // Aumentar sensibilidade da rolagem
     });
+
+    // Rolagem com o Toque
+    let startX;
+    let scrollLeft;
+
+    category.addEventListener('touchstart', function(e) {
+        startX = e.touches[0].pageX - this.offsetLeft;
+        scrollLeft = this.scrollLeft;
+    });
+
+    category.addEventListener('touchmove', function(e) {
+        e.preventDefault(); // Previne o comportamento padrão do toque
+        const x = e.touches[0].pageX - this.offsetLeft;
+        const walk = (x - startX) * 5; // Aumentar a sensibilidade da rolagem
+        this.scrollLeft = scrollLeft - walk; // Atualiza imediatamente
+    });
+});

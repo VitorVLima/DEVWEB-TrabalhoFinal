e87d65373662aa076446c1ba353b05a288203a5d
VITOR VINICIUS LIMA SILVA <vitor14_df@hotmail.com>
2024-10-31 16:15:00 -0300

    Update rolagem-divcategoria.js

--- js/index/rolagem-divcategoria.js
+++ js/index/rolagem-divcategoria.js
@@ -1,25 +1,25 @@
 const categories = document.querySelectorAll('.category');
 
-categories.forEach(category => {
-    // Rolagem com o Mouse
-    category.addEventListener('wheel', function(event) {
-        event.preventDefault(); // Previne o comportamento padrão da rolagem
-        this.scrollLeft += event.deltaY * 1.5; // Aumentar sensibilidade da rolagem
-    });
+    categories.forEach(category => {
+        // Rolagem com o Mouse
+        category.addEventListener('wheel', function(event) {
+            event.preventDefault(); // Previne o comportamento padrão da rolagem
+            this.scrollLeft += event.deltaY; // Aumenta a sensibilidade da rolagem
+        });
 
-    // Rolagem com o Toque
-    let startX;
-    let scrollLeft;
+        // Rolagem com o Toque
+        let startX;
+        let scrollLeft;
 
-    category.addEventListener('touchstart', function(e) {
-        startX = e.touches[0].pageX - this.offsetLeft;
-        scrollLeft = this.scrollLeft;
-    });
+        category.addEventListener('touchstart', function(e) {
+            startX = e.touches[0].pageX - this.offsetLeft;
+            scrollLeft = this.scrollLeft;
+        });
 
-    category.addEventListener('touchmove', function(e) {
-        e.preventDefault(); // Previne o comportamento padrão do toque
-        const x = e.touches[0].pageX - this.offsetLeft;
-        const walk = (x - startX) * 5; // Aumentar a sensibilidade da rolagem
-        this.scrollLeft = scrollLeft - walk; // Atualiza imediatamente
+        category.addEventListener('touchmove', function(e) {
+            e.preventDefault(); // Previne o comportamento padrão do toque
+            const x = e.touches[0].pageX - this.offsetLeft;
+            const walk = (x - startX) * 2; // Ajuste a sensibilidade da rolagem
+            this.scrollLeft = scrollLeft - walk; // Atualiza a posição de rolagem
+        });
     });
-});

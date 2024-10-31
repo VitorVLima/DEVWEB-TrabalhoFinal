4c510761fa252b4e5a7ff39dc607d304fc56046b
VITOR VINICIUS LIMA SILVA <vitor14_df@hotmail.com>
2024-10-31 15:17:26 -0300

    vitor

--- js/index/rolagem-divcategoria.js
+++ js/index/rolagem-divcategoria.js
@@ -1,25 +1,36 @@
 const categoryContainer = document.getElementsByClassName('category');
 
-        for (let i = 0; i < categoryContainer.length; i++) {
-            // Rolagem com o Mouse
-            categoryContainer[i].addEventListener('wheel', function(event) {
-                event.preventDefault(); // Previne o comportamento padrão da rolagem
-                this.scrollLeft += event.deltaY * 1.5; // Aumentar sensibilidade da rolagem
-            });
+    for (let i = 0; i < categoryContainer.length; i++) {
+        // Rolagem com o Mouse
+        categoryContainer[i].addEventListener('wheel', function(event) {
+            event.preventDefault(); // Previne o comportamento padrão da rolagem
+            this.scrollLeft += event.deltaY * 1.5; // Aumentar sensibilidade da rolagem
+        });
 
-            // Rolagem com o Toque
-            let startX;
-            let scrollLeft;
+        // Rolagem com o Toque
+        let startX;
+        let startY;
+        let scrollLeft;
 
-            categoryContainer[i].addEventListener('touchstart', function(e) {
-                startX = e.touches[0].pageX - this.offsetLeft;
-                scrollLeft = this.scrollLeft;
-            });
+        categoryContainer[i].addEventListener('touchstart', function(e) {
+            startX = e.touches[0].pageX - this.offsetLeft;
+            startY = e.touches[0].pageY - this.offsetTop;
+            scrollLeft = this.scrollLeft;
+        });
 
-            categoryContainer[i].addEventListener('touchmove', function(e) {
-                e.preventDefault(); // Previne o comportamento padrão do toque
-                const x = e.touches[0].pageX - this.offsetLeft;
-                const walk = (x - startX) * 3; // Aumentar a sensibilidade da rolagem
-                this.scrollLeft = scrollLeft - walk; // Atualiza imediatamente
-            });
-        }
+        categoryContainer[i].addEventListener('touchmove', function(e) {
+            e.preventDefault(); // Previne o comportamento padrão do toque
+            const x = e.touches[0].pageX - this.offsetLeft;
+            const y = e.touches[0].pageY - this.offsetTop;
+            const walkX = (x - startX) * 5; // Sensibilidade para rolagem horizontal
+            const walkY = (y - startY) * 5; // Sensibilidade para rolagem vertical
+
+            // Verifica se o movimento horizontal é maior que o vertical
+            if (Math.abs(walkX) > Math.abs(walkY)) {
+                this.scrollLeft = scrollLeft - walkX; // Rolagem lateral
+            } else {
+                // Permite a rolagem vertical se a distância vertical for maior
+                this.scrollTop += walkY; // Rolagem vertical, se necessário
+            }
+        });
+    }
